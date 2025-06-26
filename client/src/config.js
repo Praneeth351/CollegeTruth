49b0be60e229d5481f26704e32c0a2bf771913e3
Harsh51 <harshapraneeth51@gmail.com>
2025-06-26 16:56:12 +0530

    Initial commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,11 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
+let BASE_URL = process.env.REACT_APP_SERVER;
+
+if (!BASE_URL) {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    BASE_URL = "http://localhost:4000/";
+  } else {
+    BASE_URL = "https://your-backend.onrender.com/";
+  }
 }
 
 export { BASE_URL };
