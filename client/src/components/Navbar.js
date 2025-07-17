1c9f6516ac3cf2165c545a43f2d6bbb50db3303d
Harsh51 <harshapraneeth51@gmail.com>
2025-07-18 01:03:18 +0530

    Initial commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -91,7 +91,7 @@ const Navbar = () => {
             color={theme.palette.primary.main}
           >
             {/* <Link to="/" color="inherit"> */}
-              PostIt
+              CollegeTruth
             {/* </Link> */}
           </Typography>
         </HorizontalStack>
